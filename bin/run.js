4fa36f5cac67da6cda130a47cde778717a7fad34
Lipsoft <mail@lipsoft.pt>
2019-09-11 13:07:55 +0000

    cleanup and changes to templates

--- bin/run.js
+++ bin/run.js
@@ -4,11 +4,13 @@ const { render, loadTemplate } = require('../lib/common')
 const modelTemplate = loadTemplate('../../build/templates/oapiToModels/model.mustache')
 const controllerTemplate = loadTemplate('../../build/templates/oapiToControllers/controller.mustache')
 
-const petstore = common.yamlToObject('../../build/petstore.yaml')
-oapiToModels(petstore).forEach(schema => {
+// const petstore = common.yamlToObject('../../build/petstore.yaml')
+const imported = require('../build/imported/fromMysql.json')
+
+oapiToModels(imported).forEach(schema => {
   common.saveFile(`../../dist/oapiToModels/${schema.name}.php`, render(modelTemplate, schema))
 })
 
-oapiToControllers(petstore).forEach(schema => {
+oapiToControllers(imported).forEach(schema => {
   common.saveFile(`../../dist/oapiToControllers/${schema.name}.php`, render(controllerTemplate, schema))
 })
