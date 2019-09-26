a7a16fa4603884e298d7b59ba16bf91d2af9868c
Lipsoft <mail@lipsoft.pt>
2019-09-26 20:32:48 +0000

    index is working and create new template for the router

--- bin/run.js
+++ bin/run.js
@@ -1,8 +1,9 @@
-const { common, oapiToModels, oapiToControllers, oapiToViews } = require('../lib')
+const { common, oapiToModels, oapiToControllers, oapiToViews, oapiToRouter } = require('../lib')
 
 const { render, loadTemplate } = require('../lib/common')
 const modelTemplate = loadTemplate('../../build/templates/oapiToModels/model.mustache')
 const controllerTemplate = loadTemplate('../../build/templates/oapiToControllers/controller.mustache')
+const routerTemplate = loadTemplate('../../build/templates/oapiToRouter/router.mustache')
 
 // const petstore = common.yamlToObject('../../build/petstore.yaml')
 const imported = require('../build/imported/fromMysql.json')
@@ -14,6 +15,8 @@ oapiToModels(imported).forEach(schema => {
 oapiToControllers(imported).forEach(schema => {
   common.saveFile(`../../dist/app/Http/Controllers/${common.capitalize(schema.name)}Controller.php`, render(controllerTemplate, schema))
 })
+
+common.saveFile(`../../dist/router.php`, render(routerTemplate, oapiToRouter(imported)))
 
 oapiToViews(imported).forEach(schema => {
   schema.views.forEach(view => common.saveFile(`../../dist/resources/views/${schema.name}/${view.name}.blade.php`, render(view.template, view.content)))
